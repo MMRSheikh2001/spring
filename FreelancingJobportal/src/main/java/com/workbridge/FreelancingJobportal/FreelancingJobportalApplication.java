b0b199d89c1cd9acc0b7cdd2918995df22a7a45e
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-05-19 16:28:48 +0600

    1st

---
+++ FreelancingJobportal/src/main/java/com/workbridge/FreelancingJobportal/FreelancingJobportalApplication.java
@@ -0,0 +1,13 @@
+package com.workbridge.FreelancingJobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancingJobportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancingJobportalApplication.class, args);
+	}
+
+}
---
+++ FreelancingJobportal/src/test/java/com/workbridge/FreelancingJobportal/FreelancingJobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workbridge.FreelancingJobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancingJobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
