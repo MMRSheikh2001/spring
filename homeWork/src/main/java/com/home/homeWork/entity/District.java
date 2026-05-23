8a3bfcf33ce91e6636162479ad9f85dfbfec1d06
MD MAHBUBUR RAHMAN SHEIKH <mahabub.sheikh2001@gmail.com>
2026-05-23 16:34:35 +0600

    ghc

---
+++ homeWork/src/main/java/com/home/homeWork/entity/District.java
@@ -0,0 +1,23 @@
+package com.home.homeWork.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "districts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class District {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "division_id")
+    private Division division;
+}
